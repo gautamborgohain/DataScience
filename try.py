69936af73ecf21b9f641a456b64fd7f3763745bd
nightfoxbarricade <gautamborgohain90@gmail.com>
2016-04-26 13:24:44 +0800

    Added new features

--- try.py
+++
@@ -1,8 +0,0 @@
-[{'dep': 'ROOT', 'governor': 0, 'dependent': 2, 'dependentGloss': 'guys', 'governorGloss': 'ROOT'},
- {'dep': 'compound', 'governor': 2, 'dependent': 1, 'dependentGloss': 'U', 'governorGloss': 'guys'},
- {'dep': 'amod', 'governor': 4, 'dependent': 3, 'dependentGloss': 'better', 'governorGloss': 'wake'},
- {'dep': 'dep', 'governor': 2, 'dependent': 4, 'dependentGloss': 'wake', 'governorGloss': 'guys'},
- {'dep': 'case', 'governor': 7, 'dependent': 5, 'dependentGloss': 'up', 'governorGloss': 'idea'},
- {'dep': 'compound', 'governor': 7, 'dependent': 6, 'dependentGloss': 'ur', 'governorGloss': 'idea'},
- {'dep': 'nmod', 'governor': 4, 'dependent': 7, 'dependentGloss': 'idea', 'governorGloss': 'wake'},
- {'dep': 'punct', 'governor': 2, 'dependent': 8, 'dependentGloss': '!', 'governorGloss': 'guys'}]
